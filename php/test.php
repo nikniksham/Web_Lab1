c92df489de54dab754bed4059b76818a90cc630f
Nikolausus <nikniksham@gmail.com>
2023-09-07 21:37:35 +0300

    факинг ксс был побеждён

--- php/test.php
+++ php/test.php
@@ -1,3 +1,10 @@
 <?php
 
+for ($i = 1; $i <= 20; $i++) {
+    echo '<tr>\n';
+    echo '<td>The table body</td>';
+    echo '<td>with two columns</td>';
+    echo '<td>with two columns</td>';
+    echo '</tr>';
+}
 ?>
