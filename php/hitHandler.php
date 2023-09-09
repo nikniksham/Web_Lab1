600dd32106f4fdac1126e44e4938275ca42bfbcc
Nikolausus <nikniksham@gmail.com>
2023-09-09 17:58:53 +0300

    Добавлен обработчик ошибок

--- php/hitHandler.php
+++ php/hitHandler.php
@@ -2,13 +2,36 @@
 $startTime = microtime(true);
 
 try {
-    $x = (float)$_GET["x"];
-    $y = (float)$_GET["y"];
-    $r = (float)$_GET["r"];
+    $x = $_GET["x"];
+    $y = $_GET["y"];
+    $r = $_GET["r"];
     $result = "Скосил";
+    preg_match('/\-?\d+\.?\d{0,}/', $x, $matches);
+    if (count($matches) == 1 && $matches[0] == $x) {
+        $x = (float)$x;
+    } else {
+        exit("X должен быть float");
+    }
+
+    preg_match('/\-?\d+\.?\d{0,}/', $y, $matches);
+    if (count($matches) == 1 && $matches[0] == $y) {
+        $y = (float)$y;
+    } else {
+        exit("Y должен быть float");
+    }
+
+    preg_match('/\-?\d+\.?\d{0,}/', $r, $matches);
+    if (count($matches) == 1 && $matches[0] == $r) {
+        $r = (float)$r;
+
+        if ($r <= 0) {
+            exit("R должен быть больше 0");
+        }
+    } else {
+        exit("R должен быть float");
+    }
 } catch (Exception $e) {
-//    echo "Передаваемые данные должны быть типа float";
-    throw new Exception("Передаваемые данные должны быть типа float");
+    exit($e);
 }
 
 if (($x <= 0 && $y <= 0 && ($r / 2) ** 2 - $x ** 2 - $y ** 2 >= 0) || ($x <= 0 && $y >= 0 && $x >= -$r / 2 && $y <= $r) || ($x >= 0 && $y <= 0 && $x <= $r && $y >= $x / 2 - $r / 2)) {
